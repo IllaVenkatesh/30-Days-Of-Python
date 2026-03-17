27787f19bbee3d1aad6b65d52a28a26c883c356b
Venkatesh Illa <venkateshilla1818@gmail.com>
2026-03-17 21:35:21 +0530

    output added in helloworld.py

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -19,7 +19,27 @@ print(3 // 2)  # Floor division operator(//)
 print(type(10))                  # Int
 print(type(3.14))                # Float
 print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
+print(type('Venkatesh'))          # String
 print(type([1, 2, 3]))           # List
-print(type({'name': 'Asabeneh'}))  # Dictionary
+print(type({'name': 'Venkatesh'}))  # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Tuple
+
+#OUTPUT
+Hello World!
+5
+2
+6
+5
+1
+6
+1.5
+9
+1
+1
+<class 'int'>
+<class 'float'>
+<class 'complex'>
+<class 'str'>
+<class 'list'>
+<class 'dict'>
+<class 'set'>
